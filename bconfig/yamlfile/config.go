2656f49fe1335dcef17475258c02edde56b8f616
Lamber Chen <chenchenliao@qq.com>
2023-05-19 19:05:51 +0800

    feat(config): yamlfile

--- bconfig/yamlfile/config.go
+++ bconfig/yamlfile/config.go
@@ -5,12 +5,26 @@ import (
 	"fmt"
 	"go-brick/bconfig"
 	"go-brick/berror"
+	"go-brick/blog/logger"
+	"go-brick/internal/bufferpool"
+	"os"
 	"sync"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 )
 
-const defaultNamespace = "config"
+const (
+	defaultRoot = "config"
+
+	defaultDirStatic  = "static"
+	defaultDirDynamic = "dynamic"
+	defaultFilename   = "config"
+)
+
+var (
+	_Root = defaultRoot
+)
 
 // TODO: 加载自定义配置文件目录路径
 
@@ -24,39 +38,76 @@ func newDefault() *yamlFile {
 }
 
 type yamlFile struct {
-	static  sync.Map
-	dynamic sync.Map
+	static  sync.Map // map[filename]config
+	dynamic sync.Map // map[filename]config
 }
 
-func (f *yamlFile) StaticLoad(key string, namespace ...string) (bconfig.Value, error) {
-	var filename = defaultNamespace
-	if len(namespace) > 0 {
-		filename = namespace[0]
+func (f *yamlFile) StaticLoad(key string, filenames ...string) (bconfig.Value, error) {
+	var filename = defaultFilename
+	if len(filenames) > 0 {
+		filename = filenames[0]
 	}
-
-	conf, ok := f.static.Load(key)
+	// try to get from cache
+	cache, ok := f.static.Load(filename)
 	if ok {
-		return conf.(bconfig.Value), nil
+		return f.getSub(cache.(*viper.Viper), key)
 	}
-	v, err := f.loadFile("", filename)
+	// read config file
+	newData, err := f.loadFile(filename, true)
 	if err != nil {
 		return nil, err
 	}
-	v = v.Sub(key)
-	if v == nil {
-		return nil, berror.NewNotFound(nil, fmt.Sprintf("Cannot find key[%s]", key))
+	if newData == nil {
+		return nil, f.notfoundError(key)
 	}
-	f.static.Store(key, v)
+	// cache config
+	// do not check key is existing or not
+	f.static.Store(filename, newData)
 
-	return defaultValue{v: v}, nil
+	return f.getSub(newData, key)
 }
 
-func (f *yamlFile) DynamicLoad(ctx context.Context, key string, namespace ...string) (bconfig.Value, error) {
-	// TODO implement me
-	panic("implement me")
+func (f *yamlFile) DynamicLoad(ctx context.Context, key string, filenames ...string) (bconfig.Value, error) {
+	// TODO: 需要将读取配置动作加入到上下文追踪信息
+
+	var filename = defaultFilename
+	if len(filenames) > 0 {
+		filename = filenames[0]
+	}
+
+	// try to get from cache
+	cache, ok := f.static.Load(filename)
+	if ok {
+		return f.getSub(cache.(*viper.Viper), key)
+	}
+
+	// TODO: 加原子锁。堵塞等待获取解锁。解锁后再次读缓存。
+	// Load()到这里的时间间隙很短，忽略多次读同一个文件的情况。
+
+	// read config file
+	newData, err := f.loadFile(filename, true)
+	if err != nil {
+		return nil, err
+	}
+	if newData == nil {
+		return nil, f.notfoundError(key)
+	}
+
+	// run watcher
+	newData.WatchConfig()
+	newData.OnConfigChange(func(in fsnotify.Event) {
+		logger.Infra.Infow("[EVENT] config update", "event", in.String())
+	})
+
+	// cache config
+	// do not check key is existing or not
+	f.static.Store(filename, newData)
+
+	return f.getSub(newData, key)
 }
 
-func (f *yamlFile) loadFile(dir, filename string) (*viper.Viper, error) {
+func (f *yamlFile) loadFile(filename string, static bool) (*viper.Viper, error) {
+	dir := f.getDir(static)
 	v := viper.New()
 	v.AddConfigPath(dir)
 	v.SetConfigFile(filename)
@@ -65,4 +116,29 @@ func (f *yamlFile) loadFile(dir, filename string) (*viper.Viper, error) {
 		return nil, berror.Convert(err, fmt.Sprintf("Failed to load config[%s:%s]", dir, filename), nil)
 	}
 	return v, nil
+}
+
+func (f *yamlFile) getDir(static bool) string {
+	tmp := bufferpool.Get()
+	tmp.AppendString(_Root)
+	if static {
+		tmp.AppendByte(os.PathSeparator)
+		tmp.AppendString(defaultDirStatic)
+	} else {
+		tmp.AppendByte(os.PathSeparator)
+		tmp.AppendString(defaultDirDynamic)
+	}
+	return tmp.String()
+}
+
+func (f *yamlFile) getSub(v *viper.Viper, key string) (bconfig.Value, error) {
+	sub := v.Sub(key)
+	if sub == nil {
+		return nil, f.notfoundError(key)
+	}
+	return newDefaultValue(sub), nil
+}
+
+func (f *yamlFile) notfoundError(key string) error {
+	return berror.NewNotFound(nil, fmt.Sprintf("Cannot find key[%s]", key))
 }
--- bconfig/yamlfile/value.go
+++ bconfig/yamlfile/value.go
@@ -11,52 +11,46 @@ type defaultValue struct {
 	v *viper.Viper
 }
 
-func (d defaultValue) Sub(key string) bconfig.Value {
-	// TODO implement me
-	panic("implement me")
+func newDefaultValue(v *viper.Viper) bconfig.Value {
+	return &defaultValue{v: v}
 }
 
-func (d defaultValue) GetInt(key string) int {
-	// TODO implement me
-	panic("implement me")
+func (d *defaultValue) Sub(key string) bconfig.Value {
+	return &defaultValue{v: d.v.Sub(key)}
 }
 
-func (d defaultValue) GetUint(key string) uint {
-	// TODO implement me
-	panic("implement me")
+func (d *defaultValue) GetInt(key string) int {
+	return d.v.GetInt(key)
 }
 
-func (d defaultValue) GetString(key string) string {
-	// TODO implement me
-	panic("implement me")
+func (d *defaultValue) GetUint(key string) uint {
+	return d.v.GetUint(key)
 }
 
-func (d defaultValue) GetBool(key string) bool {
-	// TODO implement me
-	panic("implement me")
+func (d *defaultValue) GetString(key string) string {
+	return d.v.GetString(key)
 }
 
-func (d defaultValue) GetDuration(key string) time.Duration {
-	// TODO implement me
-	panic("implement me")
+func (d *defaultValue) GetBool(key string) bool {
+	return d.v.GetBool(key)
 }
 
-func (d defaultValue) GetIntSlice(key string) []int {
-	// TODO implement me
-	panic("implement me")
+func (d *defaultValue) GetDuration(key string) time.Duration {
+	return d.v.GetDuration(key)
 }
 
-func (d defaultValue) GetStringSlice(key string) []string {
-	// TODO implement me
-	panic("implement me")
+func (d *defaultValue) GetIntSlice(key string) []int {
+	return d.v.GetIntSlice(key)
 }
 
-func (d defaultValue) GetStringMap(key string) map[string]any {
-	// TODO implement me
-	panic("implement me")
+func (d *defaultValue) GetStringSlice(key string) []string {
+	return d.v.GetStringSlice(key)
 }
 
-func (d defaultValue) Unmarshal(rawVal any) error {
-	// TODO implement me
-	panic("implement me")
+func (d *defaultValue) GetStringMap(key string) map[string]any {
+	return d.v.GetStringMap(key)
+}
+
+func (d *defaultValue) Unmarshal(rawVal any) error {
+	return d.v.Unmarshal(rawVal)
 }

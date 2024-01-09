859528c47fdf53ccbcbf020f6b039a246f74dbd9
Lamber Chen <chenchenliao@qq.com>
2024-01-09 16:09:58 +0800

    feat(*): 补充普通集合方法
    feat(*): 补充普通集合方法

--- bstructure/bset/set_test.go
+++ bstructure/bset/set_test.go
@@ -9,6 +9,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestClone(t *testing.T) {
+	s := map[uint]struct{}{
+		1:      {},
+		2:      {},
+		3:      {},
+		999999: {},
+	}
+	assert.Equal(t,
+		map[uint]struct{}{
+			1: {}, 2: {}, 3: {}, 999999: {},
+		},
+		bset.Clone(s))
+}
+
 func TestFromSlice(t *testing.T) {
 	var s1 = []uint{1, 2, 3, 4, 5, 5, 1}
 	m1 := bset.FromSlice(s1)
@@ -58,4 +72,89 @@ func TestToSafeSet(t *testing.T) {
 	var s = []uint{1, 2, 3, 4, 5, 5, 1}
 	ss := bset.ToSafeSet(bset.FromSlice(s))
 	assert.Equal(t, []uint{1, 2, 3, 4, 5}, bslice.SortNumbers(ss.ToSlice()))
+}
+
+func TestIntersectionSet(t *testing.T) {
+	var (
+		a = map[string]struct{}{
+			"111": {},
+			"222": {},
+			"333": {},
+			"444": {},
+		}
+		b = map[string]struct{}{
+			"222": {},
+			"333": {},
+			"444": {},
+			"555": {},
+		}
+		c = map[string]struct{}{
+			"333": {},
+			"444": {},
+			"555": {},
+			"666": {},
+		}
+	)
+	assert.Equal(t,
+		map[string]struct{}{
+			"333": {},
+			"444": {},
+		},
+		bset.IntersectionSet(a, b, c))
+}
+
+func TestUnionSet(t *testing.T) {
+	var (
+		a = map[string]struct{}{
+			"111": {},
+			"222": {},
+			"333": {},
+			"444": {},
+		}
+		b = map[string]struct{}{
+			"222": {},
+			"333": {},
+			"444": {},
+			"555": {},
+		}
+		c = map[string]struct{}{
+			"333": {},
+			"444": {},
+			"555": {},
+			"666": {},
+		}
+	)
+	assert.Equal(t,
+		map[string]struct{}{
+			"111": {},
+			"222": {},
+			"333": {},
+			"444": {},
+			"555": {},
+			"666": {},
+		},
+		bset.UnionSet(a, b, c))
+}
+
+func TestComplementSet(t *testing.T) {
+	var (
+		a = map[string]struct{}{
+			"111": {},
+			"222": {},
+			"333": {},
+			"444": {},
+		}
+		b = map[string]struct{}{
+			"333": {},
+			"444": {},
+			"555": {},
+			"666": {},
+		}
+	)
+	assert.Equal(t,
+		map[string]struct{}{
+			"555": {},
+			"666": {},
+		},
+		bset.ComplementSet(a, b))
 }
